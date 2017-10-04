207dda41ef617a584b5484387bd9f1be776d4d23
Andrew McDermott <aim@frobware.com>
2017-10-04 18:47:46 +0100

    setup-vms: Remove duplicate clone
    Also drop RAM to 4GB.

--- setup-vm.sh
+++ setup-vm.sh
@@ -3,12 +3,10 @@
 for i in "$@"; do
     name=${PREFIX:?oops}-vm-${i}
     virsh pool-refresh default
-    virsh vol-clone --pool default ${CLOUD_IMG:-xenial-server-cloudimg-amd64-disk1.img} ${name}.img
-    virsh vol-resize --pool default ${name}.img +20G
     virsh vol-clone --pool default ${CLOUD_IMG:-CentOS-7-x86_64-GenericCloud.qcow2} ${name}.img
     virsh vol-resize --pool default ${name}.img +50G
     virsh pool-refresh default
-    virt-install -r 8192 \
+    virt-install -r 4096 \
 		 --os-variant=linux \
 		 -n $name \
 		 --vcpus=2 \
@@ -16,7 +14,7 @@ for i in "$@"; do
 		 --noautoconsole \
 		 --memballoon virtio \
 		 --boot hd \
-		 --disk vol=default/${name}.img,format=qcow2,bus=virtio,cache=writeback,size=${DISK_SIZE:-10} \
+		 --disk vol=default/${name}.img,format=qcow2,bus=virtio,cache=writeback,size=${DISK_SIZE:-50} \
 		 --disk vol=default/${name}-ds.iso,bus=virtio \
 		 --network network=${NETWORK:?nonet},model=virtio \
 
